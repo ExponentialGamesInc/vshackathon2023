f3da54d6387398128fc72a1a20508e716f2bb37a
AsherDubz <asheramemon@gmail.com>
2023-06-27 01:41:51 -0700

    Fixed Regular Enemy Animations

--- Assets/C#/enemyselector.cs
+++ Assets/C#/enemyselector.cs
@@ -1,16 +1,27 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class enemyselector : MonoBehaviour
 {
     public Animator animator;
+    public AnimatorController POPcontroller;
+    public AnimatorController CBAGcontroller;
+    public AnimatorController PBAGcontroller;
+
+    AnimatorController[] Controllers;
+
     private int randomInt;
     void Start()
     {
+        Controllers = new AnimatorController[] {POPcontroller, CBAGcontroller, PBAGcontroller};
+
+
         randomInt = UnityEngine.Random.Range(0, 3);
-        animator.SetInteger("EnemySelector", randomInt);
+
+        animator.runtimeAnimatorController = Controllers[randomInt];
         Debug.Log(randomInt);
     }
 
